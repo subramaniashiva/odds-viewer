b3ba99c82d396934feff29847d0447e9df574fac
subramaniashiva <vengaishiva@gmail.com>
2017-06-24 20:24:56 +0100

    feat(table): add table and table row dumb component

---
+++ src/app/components/odds-list/odds-list.component.spec.ts
@@ -0,0 +1,31 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { OddsListComponent } from './odds-list.component';
+import { OddsListItemComponent } from '../odds-list-item/odds-list-item.component';
+import data from '../../utils/data';
+
+describe('OddsListComponent', () => {
+  let component: OddsListComponent;
+  let fixture: ComponentFixture<OddsListComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ OddsListComponent, OddsListItemComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OddsListComponent);
+    component = fixture.componentInstance;
+    component.oddsDetails = data;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
