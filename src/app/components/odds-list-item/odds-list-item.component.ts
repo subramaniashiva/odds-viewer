3839e7985a07d4dbc4abc74e92c3d28ad263ae93
subramaniashiva <vengaishiva@gmail.com>
2017-06-24 23:54:50 +0100

    feat:(table-hover) add hover table to the list items

--- src/app/components/odds-list-item/odds-list-item.component.ts
+++ src/app/components/odds-list-item/odds-list-item.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { OddsListItemInterface } from '../../interfaces/oddsListItemInterface';
+import { getMaximum } from '../../utils/helper';
+
 @Component({
   selector: 'app-odds-list-item',
   templateUrl: './odds-list-item.component.html',
@@ -10,9 +12,36 @@ export class OddsListItemComponent implements OnInit {
 
   @Input() oddDetail: OddsListItemInterface;
 
+  max_details: Array<Object> = [];
+
+  hoverShown: boolean = false;
+
+  hoverTable: Array<number> = [];
+
+  hoverTableLeft: string = '0%';
+
   constructor() { }
 
   ngOnInit() {
+    this.max_details = this.oddDetail.exchanges.map((item) => ({
+      'back_all': getMaximum(item.back_all, 'price'),
+      'lay_all': getMaximum(item.lay_all, 'price')
+    }));
   }
 
+  showHoverTable(type, index) {
+    this.hoverShown = true;
+    if(type === 'back') {
+      this.hoverTable = this.oddDetail.exchanges[index].back_all.map((item) => (item.price));
+      this.hoverTableLeft = ((index + 1)*20).toString() + '%';
+    } else {
+      this.hoverTable = this.oddDetail.exchanges[index].lay_all.map((item) => (item.price));
+      this.hoverTableLeft = ((index + 1)*25).toString() + '%';
+    }
+  }
+
+  hideHoverTable() {
+    this.hoverShown = false;
+    this.hoverTable = [];
+  }
 }
